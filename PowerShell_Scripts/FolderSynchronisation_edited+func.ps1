345b36bd401ec5d5316a0b4b7444d72997f51cef
Dasha <dariademidova7@gmail.com>
2021-05-10 13:19:33 +0300

    Working with functions (in process)

---
+++ PowerShell_Scripts/FolderSynchronisation_edited+func.ps1
@@ -0,0 +1,10 @@
+﻿function Path {
+    Write-Host "Please, specify the path"
+}
+
+Path
+$TestFolder1Path = Read-Host #'C:\Users\Daria\Desktop\TestFolder1'#
+Path
+$TestFolder2Path = Read-Host #'C:\Users\Daria\Desktop\TestFolder2'#
+Get-ChildItem $TestFolder1Path -Recurse | Copy-Item -Destination $TestFolder2Path -Force
+Get-ChildItem $TestFolder2Path -Recurse | Copy-Item -Destination $TestFolder1Path -Force
