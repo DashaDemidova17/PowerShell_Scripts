033a82f9e499b747e5591ffccc4f40ddc0d0f6b8
Dasha <dariademidova7@gmail.com>
2021-05-24 09:36:15 +0300

    Updates on NameSurname and FolderSynchronisation scripts

---
+++ PowerShell_Scripts/NameSurname_1more.ps1
@@ -0,0 +1,8 @@
+﻿Clear-Host
+$name = Read-Host 'What is your name?'
+$surName = Read-Host 'What is your surname?'
+Write-Host 'Indicate the file, where we need to save it'
+$path = Read-Host
+
+$getArray = Get-Content -Path $path -Raw -ErrorAction SilentlyContinue | ConvertFrom-Json 
+$NameSurname = @($name, $surName) + @($getArray) | ConvertTo-Json | Out-File $path <#C:\Users\Daria\Desktop\Tasks\PowerShell_Scripting\NameSurname.json#>
